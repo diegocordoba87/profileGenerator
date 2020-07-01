a29a98846d94a737d01a86d36e8a8b257675f6df
Diego Cordoba <diegocordoba87@hotmail.com>
2020-06-30 21:45:12 -0400

    passed all employee.js tests

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -14,6 +14,7 @@ class Employee{
     this.name = name;
     this.id= id;
     this.email = email;
+    this.role = "Employee"
     }
     getName() {
         return this.name
@@ -25,7 +26,7 @@ class Employee{
         return this.email
     }
     getRole(){
-        return employee = new Employee
+        return this.role
     }
 }
 
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,29 @@
+const Employee = require('')
+
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Employee class
+
+class Engineer{
+    constructor(name, id, email){
+    
+    this.name = name;
+    this.id= id;
+    this.email = email;
+    
+    }
+    getName() {
+        return this.name
+      }
+    getId() {
+        return this.id
+    }
+    getEmail() {
+        return this.email
+    }
+    getRole(){
+        return employee = new Employee
+    }
+}
+
+
+module.exports = Employee;
