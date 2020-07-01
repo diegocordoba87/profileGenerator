06f038bf0e4d78fcb140b4f46be3650bf6702fb6
Diego Cordoba <diegocordoba87@hotmail.com>
2020-06-30 20:56:23 -0400

    employee.js passed 7 tests

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -2,10 +2,32 @@
 
 class Employee{
     constructor(name, id, email){
-        this.name = name;
-        this.id= id;
-        this.email = email;
+    // if (!name) {
+    // throw new Error("You are missing the name.");
+    // }
+    // if (!id) {
+    // throw new Error("You are missing the ID.");
+    // }
+    // if (!email) {
+    // throw new Error("You are missing the email.");
+    // }
+    this.name = name;
+    this.id= id;
+    this.email = email;
+    }
+    getName() {
+        return this.name
+      }
+    getId() {
+        return this.id
+    }
+    getEmail() {
+        return this.email
+    }
+    getRole(){
+        return employee = new Employee
     }
 }
+
 
 module.exports = Employee;
