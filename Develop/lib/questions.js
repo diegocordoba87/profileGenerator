ad12453216d90d6acd5c09ea37823b9340d02800
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-01 20:44:22 -0400

    added functions to jump from one type of employee to another and modified prompt to start with manager by default

--- Develop/lib/questions.js
+++ Develop/lib/questions.js
@@ -3,50 +3,63 @@ const Manager = require("./Manager");
 const Engineer = require("./Engineer");
 const Intern = require("./Intern");
 
-inquirer
+
+inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your manager's name?",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "What is your manager's ID number",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "What is your manager's email address?",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "What is your manager's office number?",
+      name: "officeNumber",
+    },
+  ])
+  .then((res) => {
+    const newManager = new Manager(res.name,res.id,res.email,res.officeNumber);
+    addMore()
+
+  });
+
+const addMore = ()=>{
+    inquirer.prompt({
+        type: "confirm",
+        message: "Besides the manager, do you want to add more team members?",
+        name: "addMore",
+        default: true
+    }).then((res)=>{
+        if(res.addMore === true){
+            empQuestions()
+        }else{
+            console.log("we will push your team members to an HTML file!")
+        }
+    })
+}
+
+
+const empQuestions = ()=>{
+    inquirer
   .prompt({
+
     type: "list",
     message: "What type of employee do you want to create a profile for?",
-    choices: ["Manager", "Engineer", "Intern"],
+    choices: ["Engineer", "Intern"],
     name: "role",
   })
   .then((answers) => {
     console.log(answers.role);
-    if (answers.role === "Manager") {
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            message: "What is your manager's name?",
-            name: "name",
-          },
-          {
-            type: "input",
-            message: "What is your manager's ID number",
-            name: "id",
-          },
-          {
-            type: "input",
-            message: "What is your manager's email address?",
-            name: "email",
-          },
-          {
-            type: "input",
-            message: "What is your manager's office number?",
-            name: "officeNumber",
-          },
-        ])
-        .then((res) => {
-          const newManager = new Manager(
-            res.name,
-            res.id,
-            res.email,
-            res.officeNumber
-          );
-          console.log(newManager);
-        });
-    
-} else if (answers.role === "Engineer") {
+    if(answers.role === "Engineer") {
     {
       inquirer
         .prompt([
@@ -78,7 +91,7 @@ inquirer
             res.email,
             res.github
           );
-          console.log(newEngineer);
+          addMore();
         });
     }
   } else if(answers.role === "Intern"){
@@ -107,7 +120,10 @@ inquirer
         ]    
         ).then(res=>{
             const newIntern = new Intern(res.name, res.id, res.email, res.github)
-            console.log(newIntern)
+            addMore();
         })
-    }
+    }}
   });
+
+}
+
