6d11a4299c4f08b6f9ea349478aa305b602f1574
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-02 12:33:37 -0400

    rendered employees object to be with the render function and added the rendered objects to the html templates

--- Develop/app.js
+++ Develop/app.js
@@ -2,9 +2,9 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const questions = require('./lib/questions')
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
@@ -13,6 +13,137 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const employees = [];
+const initial = ()=> {
+    inquirer.prompt([
+        {
+          type: "input",
+          message: "What is your manager's name?",
+          name: "name",
+        },
+        {
+          type: "input",
+          message: "What is your manager's ID number",
+          name: "id",
+        },
+        {
+          type: "input",
+          message: "What is your manager's email address?",
+          name: "email",
+        },
+        {
+          type: "input",
+          message: "What is your manager's office number?",
+          name: "officeNumber",
+        },
+      ])
+      .then((res) => {
+        const newManager = new Manager(res.name,res.id,res.email,res.officeNumber);
+        employees.push(newManager);
+        addMore()
+    
+      });
+}
+const empQuestions = ()=>{
+    inquirer
+  .prompt({
+
+    type: "list",
+    message: "What type of employee do you want to create a profile for?",
+    choices: ["Engineer", "Intern"],
+    name: "role",
+  })
+  .then((answers) => {
+    console.log(answers.role);
+    if(answers.role === "Engineer") {
+    {
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            message: "What is your Engineer's name?",
+            name: "name",
+          },
+          {
+            type: "input",
+            message: "What is your Engineer's ID number",
+            name: "id",
+          },
+          {
+            type: "input",
+            message: "What is your Engineer's email address?",
+            name: "email",
+          },
+          {
+            type: "input",
+            message: "What is your Engineer's github?",
+            name: "github",
+          },
+        ])
+        .then((res) => {
+          const newEngineer = new Engineer(res.name,res.id,res.email,res.github);
+          employees.push(newEngineer);
+          console.log(employees)
+          addMore();
+        });
+    }
+  } else if(answers.role === "Intern"){
+    {
+        inquirer.prompt([
+         {
+             type: "input",
+             message: "What is your intern's name?",
+             name: "name"
+         },
+         {
+             type: "input",
+             message: "What is your intern's ID number",
+             name: "id"
+         },
+         {
+             type: "input",
+             message: "What is your intern's email address?",
+             name: "email"
+         },
+         {
+             type: "input",
+             message: "What is your intern's school?",
+             name: "github"
+         }
+        ]    
+        ).then(res=>{
+            const newIntern = new Intern(res.name, res.id, res.email, res.github)
+            employees.push(newIntern);
+            console.log(employees)
+            addMore();
+        })
+    }}
+  });
+
+}
+const addMore = ()=>{
+    inquirer.prompt({
+        type: "confirm",
+        message: "Do you want to add more team members?",
+        name: "addMore",
+        default: true
+    }).then((res)=>{
+        if(res.addMore === true){
+            empQuestions()
+        }else{
+            console.log("we will push your team members to an HTML file!")
+            renderedTeam= render(employees)
+            fs.writeFile(outputPath,renderedTeam,function(err){
+                if(err) throw err
+            } )
+
+        }
+    })
+}
+
+
+initial()
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
