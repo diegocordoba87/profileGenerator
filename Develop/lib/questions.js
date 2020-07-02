775509fd8c2ad72d91d5d7424a11e37d67ea0f5d
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-01 20:46:45 -0400

    created function for manager's initial prompt

--- Develop/lib/questions.js
+++ Develop/lib/questions.js
@@ -3,34 +3,36 @@ const Manager = require("./Manager");
 const Engineer = require("./Engineer");
 const Intern = require("./Intern");
 
+const initial = ()=> {
+    inquirer.prompt([
+        {
+          type: "input",
+          message: "What is your manager's name?",
+          name: "name",
+        },
+        {
+          type: "input",
+          message: "What is your manager's ID number",
+          name: "id",
+        },
+        {
+          type: "input",
+          message: "What is your manager's email address?",
+          name: "email",
+        },
+        {
+          type: "input",
+          message: "What is your manager's office number?",
+          name: "officeNumber",
+        },
+      ])
+      .then((res) => {
+        const newManager = new Manager(res.name,res.id,res.email,res.officeNumber);
+        addMore()
+    
+      });
+}
 
-inquirer.prompt([
-    {
-      type: "input",
-      message: "What is your manager's name?",
-      name: "name",
-    },
-    {
-      type: "input",
-      message: "What is your manager's ID number",
-      name: "id",
-    },
-    {
-      type: "input",
-      message: "What is your manager's email address?",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "What is your manager's office number?",
-      name: "officeNumber",
-    },
-  ])
-  .then((res) => {
-    const newManager = new Manager(res.name,res.id,res.email,res.officeNumber);
-    addMore()
-
-  });
 
 const addMore = ()=>{
     inquirer.prompt({
@@ -127,3 +129,4 @@ const empQuestions = ()=>{
 
 }
 
+initial()
